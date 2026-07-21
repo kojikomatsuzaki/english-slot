ab75decbee6b00e47a32dc805a2a0eb8c83ff880
Koji Komatsuzaki <kojikomatsuzaki@gmail.com>
2026-07-22 00:15:31 +0900

    Update words.js

--- words.js
+++ words.js
@@ -1,1 +1,20 @@
+const subjects = [
+    "Tom",
+    "The cat",
+    "They",
+    "A girl"
+];
 
+const verbs = [
+    "likes",
+    "plays",
+    "eat",
+    "reads"
+];
+
+const objects = [
+    "apples",
+    "soccer",
+    "rice",
+    "a book"
+];
